90bc57f872c65bace80073d34243f3cf5f5a0f59
julinbestina <julin.bestina@yahoo.com>
2022-06-10 23:35:00 +0530

    Added code for create Contacts in Address Book

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook("Julin Bestina", "A","Kerala", "Palakkad", 678557, 8086904736l,"julin.bestina@yahoo.com","Kozhippara");
+        System.out.println(addressBook);
     }
 }
